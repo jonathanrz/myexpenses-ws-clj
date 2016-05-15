410e864389f3d11dd16af434b9dc10589394f230
Jonathan <jonathanrafaelz@gmail.com>
2016-05-15 20:48:33 -0300

    Add createdAt and updatedAt to sources

--- src/myexpenses_ws_clj/source.clj
+++ src/myexpenses_ws_clj/source.clj
@@ -4,12 +4,16 @@
             [monger.collection :as mc]
             [clojure.tools.logging :as log]
             [clojure.data.json :as json]
-            [myexpenses-ws-clj.db :as db])
+            [myexpenses-ws-clj.db :as db]
+            [clj-time.core :as time]
+            [clj-time.coerce :as tc])
 )
 
 (defn table [] "sources")
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn now [] (tc/to-long (time/now)))
 
 (defn get-all-sources []
   (response (json/write-str (mc/find-maps (db/get-db) (table))))
@@ -20,12 +24,20 @@
 )
 
 (defn create-new-source [src]
-  (response (mc/insert-and-return (db/get-db) (table) (merge src {:_id (uuid)})))
+  (let [id (uuid)]
+    (mc/insert (db/get-db) (table) (merge src {:_id id :createdAt (now) :updatedAt (now)}))
+    (get-source id)
+  )
 )
 
 (defn update-source [id src]
-    (mc/update (db/get-db) (table) {:_id id} src {:upsert true})
+  (let [origin (into {} (mc/find-maps (db/get-db) (table) {:_id id}))
+        new (merge origin src)]
+    (log/info "origin=" origin)
+    (log/info "new=" new)
+    (mc/update (db/get-db) (table) {:_id id} (merge new {:updatedAt (now)}) {:upsert false})
     (get-source id)
+  )
 )
 
 (defn delete-source [id]
