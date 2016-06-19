65a1c9d1fdba9291d016f6592405b15ebff29ca7
Jonathan <jonathanrafaelz@gmail.com>
2016-06-19 20:03:51 -0300

    More corrections for connecting to mongo via uri

--- src/myexpenses_ws_clj/db.clj
+++ src/myexpenses_ws_clj/db.clj
@@ -6,7 +6,8 @@
 (defn get-db-url []
   (get (System/getenv) "MONGODB_URI" "192.168.99.100:27017"))
 
-(defn get-db [
-  let {:keys [conn db]} (mg/connect-via-uri {:host (get-db-url)})]
-  (mg/get-db conn "my-expenses-db")
+(defn get-db []
+  (let [uri (get-db-url)
+        [conn db] (mg/connect-via-uri {:host uri})]
+  (mg/get-db conn "my-expenses-db"))
 )
