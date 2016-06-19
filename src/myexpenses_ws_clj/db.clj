acdcc610e0bdc4c36f58c72d63655407d082759a
Jonathan <jonathanrafaelz@gmail.com>
2016-06-19 19:17:17 -0300

    Add log for heroku

--- src/myexpenses_ws_clj/db.clj
+++ src/myexpenses_ws_clj/db.clj
@@ -6,5 +6,6 @@
   (get (System/getenv) "MONGODB_URI" "192.168.99.100:27017"))
 
 (defn get-db []
+  (log/info "mongo uri=" (get-db-url))
   (mg/get-db (mg/connect {:host (get-db-url)}) "my-expenses-db")
 )
