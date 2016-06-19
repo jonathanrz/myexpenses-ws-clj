d05ade9d34511a31fb6bf267dd4d318a5a24b684
Jonathan <jonathanrafaelz@gmail.com>
2016-06-19 19:08:13 -0300

    Add mongo db environment variable

--- src/myexpenses_ws_clj/db.clj
+++ src/myexpenses_ws_clj/db.clj
@@ -2,6 +2,9 @@
   (:require [monger.core :as mg])
 )
 
+(defn get-db-url []
+  (get (System/getenv) "MONGODB_URI" "192.168.99.100:27017"))
+
 (defn get-db []
-  (mg/get-db (mg/connect {:host "192.168.99.100:27017"}) "my-expenses-db")
+  (mg/get-db (mg/connect {:host (get-db-url)}) "my-expenses-db")
 )
